08c88ec8bcafda42ba4acd917435c9717e32fc1d
rnozila2197 <yjamshid2197@gmail.com>
2026-01-06 18:17:09 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# 6
+roy = [1, 2, 3]
+roy1 = [4, 5, 6]
+a = roy + roy1
+print(a)
+
+# 7
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+
+# 8
+roy = ["salom", "dunyo"]
+print(roy * 3, end=" ")
+
+# 9
+a = ["Ali", "Vali"]
+b = ["Salim", "Karim"]
+c = a + b
+print(c, end=" ")
+
+# 10
+a = 0
+print(a * 5)
+
+# # 11
+a = [1, 2]
+b = ["a", "b"]
+c = [True, False]
+d = a + b + c
+print(d,  end=" ")
